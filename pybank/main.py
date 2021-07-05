4b2ab762aa3e49ccf0763fa393cb494bf226b7ef
Maurice Jenkins <85418720+reese-oo@users.noreply.github.com>
2021-07-05 17:38:25 -0500

    Update main.py

--- pybank/main.py
+++ pybank/main.py
@@ -1,13 +1,12 @@
+#Imports the os system and csv library
 import os
 import csv
 
-
-budget_data = os.path.join("..","Resources","budget_data.csv")
-
-with open(budget_data) as budget_reader:
-
-    budget_reader = csv.reader(budget_data, delimiter=',')
-    print(budget_reader)
-
-
-    
+#Sets a variable equal to the file path to call upon a data set
+budget_file = os.path.join("Resources","budget_data.csv")
+#Sets the original variable contents equal to budget_reader to read through rows completely
+with open(budget_file) as budget_reader:
+    budget_reader = csv.reader(budget_file, delimiter = ',')
+    header = next(budget_reader)
+    print(f"{header}")
+#
